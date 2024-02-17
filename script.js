8670920635c8322c68a1152e1ffacebbdd1b578b
College-Developer <156514018+College-Developer@users.noreply.github.com>
2024-02-17 09:20:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const menuToggleBtns = document.querySelectorAll(".mobile-menu span");
+const mobileMenu = document.querySelector("ul.navbar-menu-mob");
+console.log(menuToggleBtns);
+
+menuToggleBtns.forEach((menuToggleBtn) => {
+  menuToggleBtn.addEventListener("click", () => {
+    menuToggleBtns[0].classList.toggle("hidden");
+    menuToggleBtns[1].classList.toggle("hidden");
+    console.log(menuToggleBtns[0].classList.contains("hidden"));
+    if(menuToggleBtns[0].classList.contains("hidden")){
+      // mobileMenu.style.display = "block";
+      mobileMenu.style.transform = "translateX(0)";
+    }
+    else {
+      mobileMenu.style.transform = "translateX(400px)";
+    }
+  });
+});
+
+
+
+
 var swiper = new Swiper(".swiper", {
     effect: "coverflow",
     grabCursor: true,
